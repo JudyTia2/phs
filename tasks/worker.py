3c8bf836c7ec88925b93fe5a4d22416c49dbeaac
JudyTia2 <jkl12zys@gmail.com>
2025-11-18 02:19:00 -0500

    fix type error

--- tasks/worker.py
+++ tasks/worker.py
@@ -1,12 +1,13 @@
 # tasks/worker.py
 import os
 from celery import Celery
+import ssl
 
 redis_url = os.getenv("REDIS_URL", "redis://redis:6379/0")
 celery_app = Celery("govhealth", broker=redis_url, backend=redis_url)
 celery_app.conf.task_default_queue = "default"
 celery_app.conf.task_routes = {"tasks.reporting.*": {"queue": "reports"}}
-if REDIS_URL and REDIS_URL.startswith("rediss://"):
+if redis_url and redis_url.startswith("rediss://"):
     celery_app.conf.broker_use_ssl = {
         "ssl_cert_reqs": ssl.CERT_NONE,  # Demo 用，关闭证书校验
     }
